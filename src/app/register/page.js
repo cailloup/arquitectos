5fd4fe8e3b794c8157dac7ea096bcdb2e54bbed0
cailloup <josevoyut@gmail.com>
2023-05-10 13:21:17 -0300

    agrego rutas

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,16 +1,14 @@
 import Image from 'next/image'
-import styles from './page.module.css'
-import Link from 'next/link'
+import styles from './register.module.css'
+
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <Link href="/register">
         <p>
-          La arquitectura nos vuelve mas serenos&nbsp;
+         El registro mas falopa&nbsp;
           <code className={styles.code}>src/app/page.js</code>
         </p>
-        </Link>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
