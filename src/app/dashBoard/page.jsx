14d5cdde6d2e623dd1287014774d81b4e2b8d54e
cailloup <josevoyut@gmail.com>
2023-05-14 19:46:07 -0300

    fixIt

--- src/app/dashBoard/page.jsx
+++ src/app/dashBoard/page.jsx
@@ -5,7 +5,7 @@ import {toast,ToastContainer} from "react-toastify"
 import LoadScreen from "@/components/LoadScreen"
 import styles from '@/styles/pages/DashBoard.module.css' 
 import "react-toastify/dist/ReactToastify.css";
-export default function DashBoard(){
+export default function DashBoard(){ 
     const [buildings,setBuildings] = useState((/** @type [Building] */ (null))) 
     useEffect(() => { //onPageLoad
 
