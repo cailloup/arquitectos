622f5d50821d7a2c3b4281cc997f972231c45849
cailloup <josevoyut@gmail.com>
2023-05-16 16:08:01 -0300

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -40,13 +40,27 @@ export default function sandBox(){
       }
 
     if (!isLoaded || redirect) return <LoadScreen/>
+
+    const svgMarker = {
+    
+    };
+    
     return (
         <NavBar setRedirect={setRedirect}>
             <main>
                 
                 <Map onCountySelect={handleCountyChange} onLoad={(map) => setMap(map)} >
-                    {buildings&&buildings.map( (building) => (
+                    {buildings&&buildings.map( (building,index) => (
                         <Marker
+                        icon={{
+                          path: "M-1.547 0q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+                          fillColor: index%2==0?"#1fbd93":"green" ,
+                          fillOpacity: 0.85,
+                          strokeWeight: 0,
+                          rotation: 0,
+                          scale: 2,
+                          anchor: new google.maps.Point(0, 20),
+                        }}
                         key={building.uuid}
                         position={{lat:parseFloat(building.lat),lng: parseFloat(building.longitude)}}
                         onClick={() => setSelectedBuilding(building)}
