032790851017165493c9024e79ca4e0afeab2fba
cailloup <josevoyut@gmail.com>
2023-05-16 23:32:53 -0300

    c

--- src/components/dragMenu.jsx
+++ src/components/dragMenu.jsx
@@ -4,23 +4,35 @@ import React, { useState, useEffect } from 'react';
 
 export default function DragMenu({children}){
     const [left, setLeft] = useState(window.innerWidth - 20);
-    
+    const [isdraggin, setdraggin] = useState(false);
+    const [transition,setTransition] = useState('all 350ms')
     function togleForm(){
-       
+        if(isdraggin)
+            return
+        if(left<window.innerWidth/2){
+            setLeft(window.innerWidth - 20)
+        }else{
+            setLeft(0)
+        }
+        
     }
 
     const handleMouseMove = (event) => {
         setLeft(event.clientX);
+        setdraggin(true)
+        setTransition("all 0ms")
     };
   
     const handleMouseUp = () => {
-      window.removeEventListener('mousemove', handleMouseMove);
-      window.removeEventListener('mouseup', handleMouseUp);
+        setdraggin(false)
+        setTransition("all 350ms")
+        window.removeEventListener('mousemove', handleMouseMove);
+        window.removeEventListener('mouseup', handleMouseUp);
     };
   
     const handleMouseDown = () => {
-      window.addEventListener('mousemove', handleMouseMove);
-      window.addEventListener('mouseup', handleMouseUp);
+        window.addEventListener('mousemove', handleMouseMove);
+        window.addEventListener('mouseup', handleMouseUp);
     };
   
     useEffect(() => {
@@ -30,8 +42,8 @@ export default function DragMenu({children}){
       };
     }, []);
     return (
-        <div className={styles.menuContainer} style={ {left: `min(${left}px,calc(100vw - 20px))` } }>
-            <div  className={styles.leftBar}  onMouseDown={handleMouseDown}  onClick={togleForm}>
+        <div className={styles.menuContainer} style={ {transition:transition, left: `max(0px,min(${left}px,calc(100vw - 20px)))` } }>
+            <div  className={styles.leftBar}  onMouseDown={handleMouseDown}  onMouseUp={togleForm}>
                 <div className={styles.leftBarLine}/> 
             </div>
             {children}
