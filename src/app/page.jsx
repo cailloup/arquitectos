bb866ddeeea9741dfd5ac7e8703cc598dd50bbce
cailloup <josevoyut@gmail.com>
2023-05-18 13:21:00 -0300

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,64 +1,58 @@
 "use client"
-import '@/styles/pages/map.css';
-import Map from "@/components/map";
-import { useState, useEffect,useRef } from "react";
-import { Marker,InfoWindow } from "@react-google-maps/api";
-import {useGoogleMaps} from '@/apis/googleMapsConfig'; 
-import { BuildingAPI } from "@/apis/archytectApi";
+import { useState, useEffect, useRef } from "react";
+import { Marker, InfoWindow } from "@react-google-maps/api";
 import Autosuggest from 'react-autosuggest';
+import { toast, ToastContainer } from "react-toastify";
+
+import '@/styles/pages/map.css';
+import "react-toastify/dist/ReactToastify.css";
+
+import Map from "@/components/map";
 import NavBar from '@/components/NavBar';
 import LoadScreen from "@/components/LoadScreen";
-import {DragMenu} from '@/components/dragMenu';
-import { assests } from '@/data/assest';
+import { DragMenu } from '@/components/dragMenu';
+
+import { useGoogleMaps } from '@/apis/googleMapsConfig';
+import { BuildingAPI } from "@/apis/archytectApi";
+import ArchytecstApi, { Building } from '@/apis/builddingsApi';
+import { assests } from "@/data/assest";
+
+const buildingTypes = ["C. C. Municipal","Comercial","Educativo","Esparcimiento","Historico","Hotelera","Municipal","Publico","Religioso","Urbano","Vivienda","Otro"]
+
 
 export default function sandBox(){
     const [county,setCounty] = useState(null)
     const [redirect,setRedirect] = useState(false)
     const [geocoder, setGeocoder] = useState( /** @type {window.google.maps.Geocoder | null} */ (null));
-    const isLoaded = useGoogleMaps();
-    const [selectedBuilding, setSelectedBuilding] = useState((/** @type Building */ (null)));
-    const [buildings,setBuildings] = useState((/** @type [Building] */ (null)))
+    const [selectedBuilding, setSelectedBuilding] = useState((/** @type {Building} */ (null)));
+    const [buildings,setBuildings] = useState((/** @type {[Building] || null} */ (null)))
     const [map, setMap] = useState(/** @type google.maps.Map */ (null))
-    const [allBuilds, setAllBuilds] = useState(/** @type google.maps.Map */ (null))
-    const selectorColor = useRef()
-    const selectorType = useRef()
-    const dragMenu = useRef(/** @type {DragMenu | null} */ (null) );
-
+    const [allBuildings, setAllBuildings] = useState(/** @type google.maps.Map */ (null))
+    const selectorColor = useRef(/** @type {HTMLInputElement} */ (null) )
+    const selectorType = useRef( /** @type {HTMLInputElement} */ (null))
+    const dragMenu = useRef(/** @type {DragMenu | null} */ (null) ); 
+    const archytecstApi = new ArchytecstApi()
+    /**
+     * @type {boolean}
+     */
+    const isLoaded = useGoogleMaps();
+    
     useEffect(() => { //se selecciono un partido
         if(county?.name){
-            
-          BuildingAPI.endPonts.getBuildingsByCity(county.name,(builds) => initBuilds(builds.map((build)  => {return {...build,color: generateColor(build.type)} } )) )
+          archytecstApi.getBuildingsByCity(county.name).then( buildings => {setAllBuildings(buildings); setBuildings(buildings)} )
         }
-      }, [county]);
+    }, [county]);
       
     useEffect(() => { //onBuildChange
-        if(map && selectedBuilding ){
-          const location = BuildingAPI.utils.getPosition(selectedBuilding);
-          if(map.getZoom!=17){
-            map.setZoom(17)
-          }
-          map.panTo(location)
-        }
+      if(!map || !selectedBuilding )
+        return
+      if(map.getZoom!=17){
+        map.setZoom(17)
+      }
+      map.panTo(selectedBuilding.location)
+
       }, [selectedBuilding]);
-
-      function initBuilds(builds){
-        setAllBuilds(builds)
-        setBuildings(builds)
-      }
-
-      function generateColor(type){
-        if (type == "Vivienda"){
-          return assests.colors.orange
-        }
-        if (type == "Religioso"){
-          return assests.colors.blue
-        }
-        if (type == "Publico"){
-          return assests.colors.red
-        }
-        return assests.colors.puple 
-      }
-
+      
       const handleSelectedBuildingChange = (newSelectedBuilding) => {
         const updatedBuildings = buildings.map((building) =>
           building.uuid === newSelectedBuilding.uuid ? newSelectedBuilding  : building
@@ -66,6 +60,7 @@ export default function sandBox(){
     
         setSelectedBuilding(newSelectedBuilding);
         setBuildings(updatedBuildings);
+        setAllBuildings(updatedBuildings);
       };
 
       function onLoad(mapa){
@@ -73,73 +68,99 @@ export default function sandBox(){
         setMap(mapa)
       }
 
+
       function play(src){
-        new Audio(src).play();
-        dragMenu.current.setLeft(250)
-        console.log(process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY);
+       console.log(buildings);
+        /*
+         const call = new ArchytecstApi()
+        toast.promise(
+          call.getBuildingsByCity("Partido de Villa Gesell")
+            .then(building => {console.log(building[0].refColor )}),
+          {
+            pending: 'Agregando edificio',
+            success: 'Edificio agregado correctamente 👌',
+            error: 'Hubo un error al agregar el edificio 🤯'
+          }
+        )*/
       }
-      const buildingTypes = ["C. C. Municipal","Comercial","Educativo","Esparcimiento","Historico","Hotelera","Municipal","Publico","Religioso","Urbano","Vivienda","Otro"]
+      
     if (!isLoaded || redirect) return <LoadScreen/>
    
     return (
-        <NavBar setRedirect={setRedirect}>
-            <main className='main-map'>
-              <DragMenu ref={dragMenu}>
-                <div className='filters-container'>
-                  <h1>Filtros | Detalles mas exactos?</h1><br />
-                  <button onClick={() => play(assests.audios.vela)} className='send-button' style={{float:"unset"}}> vela </button><br /><br />
-                  <select ref={selectorType} name="" id="selecttor">
-                    <option value="Publico">Publico</option>
-                    <option value="Religioso">Religioso</option>
-                    <option value="Hotelera">Hotelera</option>
-                    <option value="Vivienda">Vivienda</option>
-                    <option value="Todos">Todos</option>
-                  </select><br /><br />
-                  <button onClick={() => setBuildings( allBuilds.filter( (building ) => building.type==selectorType.current.value ||selectorType.current.value =="Todos" ) )} className='send-button' style={{float:"unset"}}> Filtrar </button>
-                  <br /><br />
-                  {selectedBuilding && 
-                    <div>
-                      <h1> {selectedBuilding.name}</h1>
-                      <img style={{width:"250px",aspectRatio:"16/9"}} src={selectedBuilding.image} alt="" />
-                        <h3>color:</h3>
-                        <select  ref={selectorColor} name="" id="color" style={{width:"250px", marginBottom:"10px", marginRight:"10px"}}>
-                          {Object.entries(assests.colors).map( ([name, value]) =>
-                            <option  key={name} value={value} >{name}</option>
-                          )}
-                        </select>
-                        <button onClick={() => handleSelectedBuildingChange({...selectedBuilding,color:selectorColor.current.value}) } className='send-button' style={{float:"unset"}}> color </button>
-                    </div>
-                  }
-                </div>
-              </DragMenu>
-                {county &&< button onClick={() => setCounty(null)} className='send-button button-back'> Volver </button>}
-                <Map onCountySelect={setCounty} onLoad={onLoad}  geocoder={geocoder} setSelectedCounty={setCounty} selectedCounty={county}>
-                    {buildings&&buildings.map( (building,index) => (
-                        <Marker
-                        icon={assests.icons.mapPoint( building.color )}
-                        key={building.uuid}
-                        label={{
-                          text: building.name,
-                          fontSize: '24px', // Aumenta el tamaño de la fuente
-                          color:"black",
-                        }}
-                        position={{lat:parseFloat(building.lat),lng: parseFloat(building.longitude)}}
-                        onClick={() => setSelectedBuilding(building)}
-                        />
-                    ) )}
-                    
-                    {selectedBuilding && (
-                        <InfoWindow
-                        position={{lat:parseFloat(selectedBuilding.lat),lng: parseFloat(selectedBuilding.longitude)}}
-                        onCloseClick={() => setSelectedBuilding(null)}
-                        >
-                            <InfoWindowContent place={selectedBuilding} />
-                        </InfoWindow>
-                    )}
-                    <SearchBar setSelectedPlace={setSelectedBuilding} buildings={buildings} ></SearchBar>
-                </Map>
-            </main>  
-        </NavBar>
+      <>
+      <ToastContainer
+              position="top-center"
+              autoClose={2000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="light"
+            />
+            <NavBar setRedirect={setRedirect}>
+          
+          <main className='main-map'>
+            <DragMenu ref={dragMenu}>
+              <div className='filters-container'>
+                <h1>Filtros | Detalles mas exactos?</h1><br />
+                <button onClick={() => play(assests.audios.vela)} className='send-button' style={{float:"unset"}}> vela </button><br /><br />
+                <select ref={selectorType} name="" id="selecttor">
+                  <option value="Publico">Publico</option>
+                  <option value="Religioso">Religioso</option>
+                  <option value="Hotelera">Hotelera</option>
+                  <option value="Vivienda">Vivienda</option>
+                  <option value="Todos">Todos</option>
+                </select><br /><br />
+                <button onClick={() => setBuildings( allBuildings.filter( (building ) => building.type==selectorType.current.value ||selectorType.current.value =="Todos" ) )} className='send-button' style={{float:"unset"}}> Filtrar </button>
+                <br /><br />
+                {selectedBuilding && 
+                  <div>
+                    <h1> {selectedBuilding.name}</h1>
+                    <img style={{width:"250px",aspectRatio:"16/9"}} src={selectedBuilding.image} alt="" />
+                      <h3>color:</h3>
+                      <select  ref={selectorColor} name="" id="color" style={{width:"250px", marginBottom:"10px", marginRight:"10px"}}>
+                        {Object.entries(assests.colors).map( ([name, value]) =>
+                          <option  key={name} value={value} >{name}</option>
+                        )}
+                      </select>
+                      <button onClick={() =>{ selectedBuilding.refColor=selectorColor.current.value ; handleSelectedBuildingChange(selectedBuilding)}} className='send-button' style={{float:"unset"}}> color </button>
+                  </div>
+                }
+              </div>
+            </DragMenu>
+              {county &&< button onClick={() => setCounty(null)} className='send-button button-back'> Volver </button>}
+              <Map onCountySelect={setCounty} onLoad={onLoad}  geocoder={geocoder} setSelectedCounty={setCounty} selectedCounty={county}>
+                  {buildings&&buildings.map( (building) => (
+                      <Marker
+                      icon={assests.icons.mapPoint( building.refColor )}
+                      key={building.uuid}
+                      label={{
+                        text: building.name,
+                        fontSize: '24px', // Aumenta el tamaño de la fuente
+                        color:"black",
+                      }}
+                      position={building.location}
+                      onClick={() => setSelectedBuilding(building)}
+                      />
+                  ) )}
+                  
+                  {selectedBuilding && (
+                      <InfoWindow
+                      position={selectedBuilding.location}
+                      onCloseClick={() => setSelectedBuilding(null)}
+                      >
+                      <InfoWindowContent place={selectedBuilding} />
+                      </InfoWindow>
+                  )}
+                  <SearchBar setSelectedPlace={setSelectedBuilding} buildings={buildings} ></SearchBar>
+              </Map>
+          </main>  
+      </NavBar>
+      </>
+        
     )
 }
 
@@ -244,16 +265,20 @@ const SearchBar = ({setSelectedPlace,buildings}) => {
     );
   };
 
-const InfoWindowContent = ( {place} ) => (
+
+/**
+ * @param {{ place: Building }} props - Propiedades del componente.
+ * @returns {JSX.Element} El contenido del InfoWindow.
+ */
+const InfoWindowContent = (  {place} ) => (
   <div className="buildingCard">
     
     <img className="buildingPicture" src={place.image} alt="" />
-    
     <div className="buildingDescription">
       <p>{place.name}</p>
       <p>Año: {place.builtDate}</p>
       <p>Constructor: {place.architect}</p>
-      <p>Ubicacion: {place.location}</p>
+      <p>Ubicacion: {place.address}</p>
       <p>Estilo: {place.style}</p>
       <p>Tipo: {place.type}</p>
     </div>
