fa262dbcbb6b81240f937d456a2f0591f623127c
cailloup <josevoyut@gmail.com>
2023-05-24 14:23:17 -0300

    Update page.jsx

--- src/app/themes/page.jsx
+++ src/app/themes/page.jsx
@@ -14,19 +14,20 @@ export default function test(){
 
                 <div style={{display:"grid", gridTemplateColumns:"1fr 1fr",gap:"30px"}}>
                     <div>
-                    <Select onChange={(e) => {sessionStorage.setItem("theme", (e.currentTarget.value));location. reload()}} style={{ width: "300px"}}>
-                        <option value={"default"}> selecciona tema cabeza </option>
+                    <Select onChange={(e) => {sessionStorage.setItem("theme", (e.currentTarget.value));location. reload()}} style={{ width: "500px"}}>
+                        <option value={"default"}> seleccione Tema</option>
                         <option value={"default"}> default </option>
                         <option value={"blueDark"}> blueDark </option>
                         <option value={"dark"}> Dark </option>
+                    </Select><br /><br />
+                    <Select onChange={(e) => {sessionStorage.setItem("nav", (e.currentTarget.value))}} style={{ width: "500px"}}>
+                        <option value={false}> Seleccione color para la barra de navegacion</option>
+                        <option value={true}>  color primario </option>
+                        <option value={false}> color secundario </option> 
                     </Select>
                     </div>
                     <div>
-                    <Select onChange={(e) => {sessionStorage.setItem("nav", (e.currentTarget.value))}} style={{ width: "300px"}}>
-                        <option value={false}> Seleccionar color para barra </option>
-                        <option value={false}> color secundario </option>
-                        <option value={true}>  color primario </option>
-                    </Select>
+                    
                     </div>
                 </div>
                 
